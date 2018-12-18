42fb0b8cf6a501e7bc34fb506aeb09e52d1c7a7a
Angela Jiang <to.ajiang@gmail.com>
2018-12-18 09:02:40 -0700

    Use categorical y on evaluate

--- scripts/selective_backprop.py
+++ scripts/selective_backprop.py
@@ -1,9 +1,10 @@
 
 # SB imports
 from importance_sampling.training import SB
-from keras.datasets import cifar10
 
 # Model imports
+import keras
+from keras.datasets import cifar10
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
@@ -14,6 +15,8 @@ from keras.layers import Activation
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.normalization import BatchNormalization
+
+num_classes = 10
 
 def base_model(num_classes, x_train):
 
@@ -43,24 +46,8 @@ def base_model(num_classes, x_train):
 
     return model
 
-def build_small_nn(input_shape, output_size):
-    model = Sequential([
-        Dense(40, activation="tanh", input_shape=input_shape),
-        Dense(40, activation="tanh"),
-        Dense(output_size),
-        Activation("softmax")
-    ])
-
-    model.compile(
-        loss="categorical_crossentropy",
-        optimizer="adam",
-        metrics=["accuracy"]
-    )
-
-    return model
-
 (x_train, y_train), (x_val, y_val) = cifar10.load_data()
-model = base_model(10, x_train)
+model = base_model(num_classes, x_train)
 model.compile(
     optimizer="adam",
     loss="categorical_crossentropy",
@@ -70,9 +57,10 @@ model.compile(
 SB(model).fit(
     x_train, y_train,
     batch_size=128,
-    epochs=10,
+    epochs=50,
     verbose=2,
     validation_data=(x_val, y_val)
 )
 
-model.evaluate(x_val, y_val)
+y_val_categorical = keras.utils.to_categorical(y_val, num_classes)
+model.evaluate(x_val, y_val_categorical)
